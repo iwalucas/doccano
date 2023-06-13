78292b9a6e0cc7e0e152ccc60001c993b7a7d0f2
Lucas Costa <teppss@gmail.com>
2023-06-13 23:06:04 +0100

    Revert "Revert "Filename filter""

--- frontend/components/utils/DropDown.vue
+++ frontend/components/utils/DropDown.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-menu offset-y open-on-hover>
+  <v-menu offset-y open-on-hover open-on-click>
     <template #activator="{ on }">
       <v-btn color="primary text-capitalize" v-on="on">
         {{ title }}
@@ -7,10 +7,26 @@
       </v-btn>
     </template>
     <v-card class="dropdown">
+      <template>
+        <v-text-field
+          v-model="filenameSearch"
+          :prepend-inner-icon="mdiMagnify"
+          :label="$t('generic.search')"
+          single-line
+          hide-details
+          filled
+          @click.stop
+        />
+      </template>
       <v-list>
-        <v-list-item v-for="(text, index) in content" :key="index">
+        <v-list-item key="0" @click="handleItemClick(0)">
           <v-list-item-content>
-            <v-list-item-title>{{ text }}</v-list-item-title>
+            <v-list-item-title>None</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item v-for="item in content" :key="item.id" @click="handleItemClick(item.id)">
+          <v-list-item-content>
+            <v-list-item-title>{{ item.text }}</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
@@ -20,7 +36,7 @@
 
 <script lang="ts">
 import Vue from 'vue'
-import { mdiMenuDown } from '@mdi/js'
+import { mdiMenuDown, mdiMagnify } from '@mdi/js'
 
 export default Vue.extend({
   name: 'Dropdown',
@@ -28,18 +44,40 @@ export default Vue.extend({
   props: {
     title: {
       type: String,
-      default: 'Actions'
+      default: 'Dropdown'
     },
     content: {
       type: Array,
       default: () => [],
+      required: true
+    },
+    clickEvent: {
+      type: Function,
+      required: true
+    },
+    searchEvent: {
+      type: Function,
       required: true
     }
   },
 
   data() {
     return {
-      mdiMenuDown
+      filenameSearch: null,
+      mdiMenuDown,
+      mdiMagnify
+    }
+  },
+
+  methods: {
+    handleItemClick(key: number) {
+      this.clickEvent('' + key)
+    }
+  },
+
+  watch: {
+    filenameSearch(newValue) {
+      this.searchEvent(newValue)
     }
   }
 })
@@ -48,6 +86,7 @@ export default Vue.extend({
 <style>
 .dropdown {
   max-height: 300px;
+  width: 800px;
   overflow-y: auto;
 }
 </style>
