738a3d51f1f78d67e20e252fa4967bd5b55a01ef
Lucas Costa <teppss@gmail.com>
2023-06-13 09:56:52 +0100

    Update admin.py

--- backend/labels/admin.py
+++ backend/labels/admin.py
@@ -6,10 +6,13 @@ from .models import BoundingBox, Category, Segmentation, Span, TextLabel
 class SpanAdmin(admin.ModelAdmin):
     list_display = ("example", "label", "start_offset", "user")
     ordering = ("example",)
+    list_filter = ("label")
+    search_fields = ("example__id",)
 
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ("example", "label", "user")
+    list_filter = ("label")
     ordering = ("example",)
 
 
@@ -21,10 +24,12 @@ class TextLabelAdmin(admin.ModelAdmin):
 class BoundingBoxAdmin(admin.ModelAdmin):
     list_display = ("example", "label", "user", "x", "y", "width", "height")
     ordering = ("example",)
+    list_filter = ("label")
 
 
 class SegmentationAdmin(admin.ModelAdmin):
     list_display = ("example", "label", "user", "points")
+    list_filter = ("label")
     ordering = ("example",)
 
 
